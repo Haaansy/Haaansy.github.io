4225b78cdc8a77f956b2cfa71e7c45ebb25e4bff
Haaansy <hansianbondoc@gmail.com>
2022-02-02 19:45:55 +0800

    update

--- scripts/hansy.js
+++ scripts/hansy.js
@@ -6,3 +6,24 @@ function copyText(evt) {
   navigator.clipboard.writeText(Text);
 
 }
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+      const square = entry.target.querySelector('.animate');
+
+      if (entry.isIntersecting) {
+          square.classList.add('animate-object');
+          return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      square.classList.remove('animate-object');
+  });
+});
+
+let target = '.animation-wrapper';
+document.querySelectorAll(target).forEach((i) => {
+    if (i) {
+        observer.observe(i);
+    }
+});
